6f98e01547d7d1b72a8246d1d7e026c6274243c9
priii <get.priyab@gmail.com>
2017-03-10 19:27:20 -0800

    cssmin

--- gruntfile.js
+++ gruntfile.js
@@ -1,22 +1,44 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-    imagemin: {
-      dynamic:{
-        options:{
-          optimizationLevel: 7
-            },
-        files: [{
-          expand: true,
-          cwd:'views/images/',
-          src:['*.{png,jpg}'],
-          dest:'dist/views/images/'
-        }]
-      }
-  }
-});
+        // minify js files
+        uglify: {
+          build: {
+            src: 'views/js/main.js',
+            dest: 'dist/views/js/main.min.js'
+          }
+        },
+        // minifying the css files
+        cssmin: {
+          dist: {
+            files: [{
+              expand: true,
+              cwd:'css/',
+              src: ['*.css','!*.min.css'],
+              dest: 'dist/css',
+              ext: '.min.css'
+            }]
+          }
+        },
+        // minifying the images
+        imagemin: {
+            dynamic: {
+                options: {
+                    optimizationLevel: 7
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'views/images/',
+                    src: ['*.{png,jpg}'],
+                    dest: 'dist/views/images/'
+                }]
+            }
+        }
+    });
     grunt.loadNpmTasks('grunt-contrib-imagemin');
-    grunt.registerTask('default', ['imagemin']);
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.registerTask('default', ['imagemin','cssmin','uglify']);
 
 };
